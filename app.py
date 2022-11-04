98eec5f8140b878b8884a05b6d13b44a66aeeead
Erezbardavid <80418649+Erezbardavid@users.noreply.github.com>
2022-11-04 19:16:01 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    name = "erez"
+    return render_template("index.html", name=name)
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
